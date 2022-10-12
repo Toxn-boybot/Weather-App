cb23f85cf5f4b80520976f862edd287d484b3aca
Toxn-boybot <star.chester00@gmail.com>
2022-10-12 12:27:13 +0200

    added toastr js

--- js/master.js
+++ js/master.js
@@ -1,12 +1,38 @@
-//Make the refresh button spin on click 
+//Make the refresh button spin on click
 function spinning() {
-    const spinner = document.getElementById("spinner");
-    spinner.classList.add("spinner");
-    const refreshButton = document.getElementById("refreshButton");
-    refreshButton.blur();
-    //remove the spin class again
-    const spinTime = setTimeout(RemoveSpin, 1000);
-    function RemoveSpin() {
-        spinner.classList.remove("spinner");
-    }
+  const spinner = document.getElementById("spinner");
+  spinner.classList.add("spinner");
+  //Show notification when clicking the refresh button using toastr js
+  const refreshButton = document.getElementById("refreshButton");
+  refreshButton.blur();
+  //remove the spin class again
+  const spinTime = setTimeout(RemoveSpin, 1000);
+  function RemoveSpin() {
+    spinner.classList.remove("spinner");
+  }
 }
+
+// refreshButton.onclick = function () {
+//         toastr.success("Item added to your cart");
+// }
+refreshButton.addEventListener("click", function () {
+  toastr.success("Item added to your cart");
+});
+
+toastr.options = {
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: false,
+  positionClass: "toast-top-right",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut",
+};
